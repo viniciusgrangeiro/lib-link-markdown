b74b3dc8a4341c680dc2a117c85ec4cbaba60393
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2022-09-24 13:30:01 -0300

    tratando erros da função pega arquivo

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
+function trataErro(err){
+    throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'));
+}
+
+function pegaArquivo(pathFile) {
+    const encoding = 'utf-8';
+
+    fs.readFile(pathFile, encoding, (err, text) =>{
+        if(err){
+            trataErro(err);
+        }
+        console.log(chalk.green(text))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
