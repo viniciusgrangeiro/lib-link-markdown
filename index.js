ccee6d476a15bf185113e33f734e59f74721b5cd
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2022-09-27 16:21:22 -0300

    criando uma funcao async

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ function trataErro(err){
     throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'));
 }
 
-function pegaArquivo(pathFile) {
-    const encoding = 'utf-8';
+// async/await
 
-    fs.readFile(pathFile, encoding, (err, text) =>{
-        if(err){
-            trataErro(err);
-        }
-        console.log(chalk.green(text))
-    })
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8';
+
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    
+        console.log(chalk.green(texto));
+    } catch (erro){
+        trataErro(erro);
+    }
+
 }
 
+
+// promisses com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro(erro))
+// }
+
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos');
